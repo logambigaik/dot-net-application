201552409bb6c9203d03ddd0eefe2eeb5a6ac107
root <root@ip-172-31-24-194.ec2.internal>
2021-02-23 21:11:51 +0000

    first commit

---
+++ opt/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace opt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
